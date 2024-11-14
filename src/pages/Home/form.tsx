5a08c1ffccaf524ed983cc79ff546127f7a7466c
ArtemBusyhin <a.busyhin@urancompany.com>
2024-11-14 15:46:31 +0100

    init commit

---
+++ src/pages/Home/form.tsx
@@ -0,0 +1,59 @@
+// App.js
+import { Network, TatumSDK, Ethereum } from "@tatumio/tatum";
+import React, { useEffect, useRef, useState } from "react";
+
+function Form() {
+  const [inputValue, setInputValue] = useState(""); // State to hold the input value
+  const [labelText, setLabelText] = useState(""); // State to hold the label text
+
+  const tatum = useRef<Ethereum>(null);
+
+  useEffect(() => () => tatum.current.destroy(), []);
+
+  const handleGetBalance = async () => {
+    tatum.current = await TatumSDK.init<Ethereum>({
+      network: Network.ETHEREUM,
+      apiKey: { v4: "t-6735ba221ef7601aa027d025-26d3a0805e994c5883b69216" },
+      verbose: true,
+    });
+
+    const balance = await tatum.current.address.getBalance({
+      addresses: [inputValue],
+    });
+    const balanceData = balance.data.filter(
+      (asset) => asset.asset === "ETH"
+    )[0];
+
+    setLabelText(`Balance: ${balanceData.balance}`);
+  };
+
+  const handleChangeWalletAddress = (
+    e: React.JSX.TargetedEvent<HTMLInputElement>
+  ) => {
+    if (e.target instanceof HTMLInputElement) {
+      setInputValue(e.target.value);
+    }
+  };
+
+  return (
+    <div>
+      <p>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChangeWalletAddress}
+          placeholder="Enter ETH wallet address to get balance"
+          style={{ padding: "5px", width: "320px" }}
+        />
+      </p>
+      <button onClick={handleGetBalance} style={{ padding: "5px" }}>
+        Click Me
+      </button>
+      <p style={{ padding: "5px", fontSize: "16px", fontWeight: "bold" }}>
+        {labelText}
+      </p>
+    </div>
+  );
+}
+
+export default Form;
